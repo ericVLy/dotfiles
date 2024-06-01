3b84f4e73db6816f474639b5636892c7dd54f2c2
linye <ericisnotrealname@protonmail.com>
2024-06-01 21:58:18 +0800

    deleted:    lvim/  	deleted:    nvim/  	deleted:    nvim_deprecated/  	deleted:    vim/.vimrc  	deleted:    zsh/

--- nvim/.config/nvim/lua/plugins/user/plugins/git_.lua
+++
@@ -1,8 +0,0 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
-return {
-  {
-    "FabijanZulj/blame.nvim",
-    config = function() require("blame").setup() end,
-  },
-}
